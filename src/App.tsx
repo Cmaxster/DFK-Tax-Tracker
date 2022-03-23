9f884a206057228da3aed54565d023dd7c2a9090
Chad Elston <cmaxster@gmail.com>
2022-03-23 08:16:26 -0400

    Rolling back to try new approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import * as crypto from '@harmony-js/crypto';
+// import * as utils from '@harmony-js/utils';
+import * as core from '@harmony-js/core';
+import {Buffer} from 'buffer';
+
+const { Harmony } = require('@harmony-js/core');
+
+const {
+  ChainID,
+  ChainType,
+  hexToNumber,
+  numberToHex,
+  fromWei,
+  Units,
+  Unit,
+} = require('@harmony-js/utils');
+
+const hmy = new Harmony(
+  'https://api.s0.b.hmny.io/',
+  {
+      chainType: ChainType.Harmony,
+      chainId: ChainID.HmyTestnet,
+  },
+);
+
+var blockTest:Number;
+
+hmy.blockchain
+  .getBalance({ address: 'one103q7qe5t2505lypvltkqtddaef5tzfxwsse4z7' })
+  .then((response:any) => {
+    blockTest = fromWei(hexToNumber(response.result), Units.one);
+    //console.log('balance in ONEs: ' + fromWei(hexToNumber(response.result), Units.one));
+  });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Testing Harmony Blockchain API..</h1>
+        <p>{blockTest}</p>
+      </main>
     </div>
   );
 }
